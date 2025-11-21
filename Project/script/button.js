24712624c7ae22a17237f69be4723fc97bb66fda
PHV <hoangvu180225@gmail.com>
2025-11-21 12:39:54 +0700

    Doi giao dien login,logout

--- Project/script/button.js
+++ Project/script/button.js
@@ -1,3 +1,42 @@
+// Chọn đúng các div theo class
+const guest = document.querySelector(".header-right.guest");
+const user  = document.querySelector(".header-right.user");
+
+// Chọn các nút
+const btnLogin    = document.getElementById("Login-Btn");
+const btnRegister = document.getElementById("Register-Btn");
+const btnLogout = document.getElementById("Logout-Btn");
+
+// Khi bấm Đăng nhập hoặc Đăng ký → chuyển giao diện
+btnLogin.addEventListener("click", switchToUser);
+btnRegister.addEventListener("click", switchToUser);
+btnLogout.addEventListener("click", switchToGuest);
+
+function switchToUser() {
+    guest.style.display = "none";
+    user.style.display = "flex";
+
+    // Lưu trạng thái để reload trang vẫn giữ giao diện user
+    localStorage.setItem("isLogin", "true");
+}
+
+localStorage.setItem("isLogin", "false");
+
+function switchToGuest() {
+    guest.style.display = "flex";
+    user.style.display = "none";
+
+    localStorage.setItem("isLogin", "false");
+}
+
+if(localStorage.getItem("isLogin") === "true") {
+    guest.style.display = "none";
+    user.style.display  = "flex";
+} else {
+    guest.style.display = "flex";
+    user.style.display  = "none";
+}
+
 const category_btns = document.querySelectorAll('.category-btn');
 
 category_btns.forEach(btn => {
@@ -10,3 +49,4 @@ category_btns.forEach(btn => {
         this.classList.add('active');
     });
 });
+
