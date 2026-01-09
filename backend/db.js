e52817d747fc586d4408c0fb0e6ddff7e9565fc5
Vuong <vtt0103@gmail.com>
2026-01-09 22:08:42 +0700

    fix db

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: '123456',
-    database: 'library_db',
+    database: 'librarymanagement',
     multipleStatements: true // Quan trọng để chạy các script dài
 });
 
